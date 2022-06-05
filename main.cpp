00a556a4e6ede7b8b467dfa6e32a116b4bd4415c
beiklive <1376896787@qq.com>
2022-06-04 19:55:37 -0700

    reset to none
    Signed-off-by: beiklive <1376896787@qq.com>

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
-#include <Contacts.h>
-#include <ContactApp.h>
 #include <spdlog/spdlog.h>
 
 
 
 int main(){
     // spdlog::set_level(spdlog::level::debug);
-    MyApp app;
-    app.run();
+
     return 0;
 }
